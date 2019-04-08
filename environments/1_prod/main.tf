89c0304ff59eebad74b73302eb167d5b0e4e1913
Tobi <trahloff@deloitte.de>
2019-04-08 14:30:54 +0200

    wörks

--- environments/1_prod/main.tf
+++ environments/1_prod/main.tf
@@ -16,8 +16,9 @@ locals {
     Managed     = "Terraform"
   }
 
-  urls = [
-    "sonninpark.de",
+  main_url = "sonninpark.de"
+
+  alternative_urls = [
     "sonninpark.com",
     "sonninstr.de",
     "sonninstrasse.de",
@@ -28,6 +29,11 @@ locals {
     "www.sonninstrasse.de",
     "www.wo-ist-die-sonninstr-nr-12.de",
   ]
+
+  urls = [
+    "${local.main_url}",
+    "${local.alternative_urls}",
+  ]
 }
 
 ##################################################################################################
@@ -35,28 +41,44 @@ locals {
 ##################################################################################################
 
 module "certificate" {
-  source                            = "git::https://github.com/cloudposse/terraform-aws-acm-request-certificate.git?ref=0.1.3"
-  domain_name                       = "${local.urls[0]}"
+  source = "git::https://github.com/cloudposse/terraform-aws-acm-request-certificate.git?ref=0.1.3"
+
+  domain_name = "${local.main_url}"
+
   # process_domain_validation_options = "true"
-  ttl                               = "300"
-  subject_alternative_names         = ["${local.urls}"]
+  # validation_method                 = "EMAIL"
+  ttl = "300"
+
+  subject_alternative_names = ["${local.alternative_urls}"]
+
+  tags = {
+    Name = "sonnin"
+  }
 }
 
-# module "cdn" {
-#   source                   = "git::https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn.git?ref=master"
-#   namespace                = "${var.project}"
-#   stage                    = "${var.environment}"
-#   compress                 = true
-#   use_regional_s3_endpoint = true
-#   name                     = "${local.prefix}-cdn"
-#   acm_certificate_arn      = "${module.certificate.arn}"
-#   aliases                  = ["${local.urls}"]
-#   parent_zone_name         = "${local.urls[0]}"
-# }
+module "cdn" {
+  source                   = "git::https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn.git?ref=master"
+  namespace                = "${var.project}"
+  stage                    = "${var.environment}"
+  compress                 = true
+  use_regional_s3_endpoint = true
+  name                     = "${local.prefix}-cdn"
+  acm_certificate_arn      = "${module.certificate.arn}"
+  aliases                  = "${local.urls}"
+  parent_zone_name         = "${local.main_url}"
 
-# resource "aws_s3_bucket_object" "index" {
-#   bucket = "${module.cdn.s3_bucket}"
-#   key    = "index.html"
-#   source = "statics/index.html"
-#   etag   = "${md5("statics/index.html")}"
-# }
+  tags = "${merge(
+    local.tags,
+    map(
+      "Comment", "${local.prefix}-cdn"
+    )
+  )}"
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket       = "${module.cdn.s3_bucket}"
+  key          = "index.html"
+  source       = "statics/index.html"
+  content_type = "text/html"
+  etag         = "${md5("statics/index.html")}"
+}
